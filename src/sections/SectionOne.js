572cf2574fb14ec4d4402d022f16a00493a2d2df
davidnjoroge <njorogedavid13@gmail.com>
2021-11-16 09:28:29 +0300

    add storybook and home page skeleton

---
+++ src/sections/SectionOne.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+export function SectionOne() {
+    return (
+      <section className="ui-section-hero" style={{background: '#EEF9FF'}}>
+        <div className="ui-layout-container">
+          <div className="ui-section-hero__layout ui-layout-grid ui-layout-grid-2">
+            <div>
+              <h1>Design better.</h1>
+              <p className="ui-text-intro">Lorem ipsum dolor sit amet, consectetur lit, enimad eiusmod tempor incididunt ut labore et dolore magna aliqua ullamco laboris nisi.</p>
+              <div className="ui-component-cta ui-layout-flex">
+                <a href="#" role="link" aria-label="#" className="ui-component-button ui-component-button-normal ui-component-button-primary">Get Started for Free</a>
+                <p className="ui-text-note"><small>30 days free trial.</small></p>
+              </div>
+            </div>
+            <img src="https://res.cloudinary.com/uisual/image/upload/assets/devices/ipad.png" loading="lazy" alt="#" className="ui-image-half-right" />
+          </div>
+        </div>
+      </section>
+    )
+}
+
+
+
+
+// #EEF9FF
