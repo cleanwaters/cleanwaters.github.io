b7bf29a775220ec4193261bfb2a72672c83f3a3d
davidnjoroge <njorogedavid13@gmail.com>
2021-11-16 10:02:42 +0300

    start navbar styling

---
+++ src/components/navbar-items-data.js
@@ -0,0 +1,30 @@
+export const navUrls = [
+    {
+        title: 'Home',
+        url: '/url'
+    },
+    {
+        title: 'Our solution',
+        url: '/url'
+    },
+    {
+        title: 'Case studies',
+        url: '/url'
+    },
+    {
+        title: 'About us',
+        url: '/url'
+    },
+    {
+        title: 'Our partners',
+        url: '/url'
+    },
+    {
+        title: 'Back to the bush',
+        url: '/url'
+    },
+    {
+        title: 'Contact',
+        url: '/url'
+    },
+]
---
+++ src/components/styled-shared.js
@@ -0,0 +1,15 @@
+import styled, { css } from 'styled-components'
+
+export const StyledNavWrapper = styled.nav`
+
+    box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.08);
+    border-radius: 150px;
+`
+
+export const StyledNavLink = styled.a`
+font-family: Poppins;
+font-style: normal;
+font-weight: 500;
+font-size: 16px;
+line-height: 24px;
+`
