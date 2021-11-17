44623f1c03fb20b11082bd8e32ccc01e3e0a6bac
Fauzan Taufik <fauzantaufik178@gmail.com>
2021-11-17 23:39:22 +0700

    section 5

--- src/sections/SectionFive.js
+++ src/sections/SectionFive.js
@@ -2,15 +2,15 @@ import { StyledLayoutContainer, StyledSection } from "../components/styled-share
 import styled from "styled-components";
 
 
-export const StyledSectionOneDetails = styled.h2`
+export const StyledSectionOneDetails = styled.div`
 font-weight: bold;
-// font-size: 48px;
+font-size: 48px !important;
 line-height: 61px;
 color: #081F47;
 margin-bottom: 1em;
 
 span {
-color: #00C0FF;
+    color: #00C0FF;
 }
 `
 
@@ -18,37 +18,58 @@ export const StyledWhatWeDoDetails = styled.div`
     border-left: 1em solid #00C0FF !important;
 `
 
+export const StyledSectionFourContainer = styled.div`
+padding:0px 22vw;
+font-size:32px;
+
+h3 {
+    font-size:36px;
+}
+
+div > p {
+    margin-bottom:0px;
+}
+
+div {
+    margin: 2.4rem 0px;
+}
+`
+
 export function SectionFive() {
     return (
         <StyledSection>
             <StyledLayoutContainer className="px-4">
-                <StyledSectionOneDetails> <span>How it </span>works?</StyledSectionOneDetails>
+                <StyledSectionFourContainer>
+                    <StyledSectionOneDetails> <span>How it </span>works?</StyledSectionOneDetails>
+                    
 
-                <h3 ><span className="text-blue">
-                    Patented heat-exchange technology in all
-                </span> our products</h3>
+                    <h3 ><span className="text-blue">
+                        Patented heat-exchange technology in all
+                    </span> our products</h3>
 
-                <p>
-                    At the heart of Watergen’s product line lies the unique, patented GENius heat exchange technology.
+                    <div>
+                        <p>
+                            At the heart of Watergen’s product line lies the unique, patented GENius heat exchange technology.
 
-                </p>
-                <p>
-                    It enables the air around us to turn into pure, fresh drinking water – energy efficiently.
-                </p>
+                        </p>
+                        <p>
+                            It enables the air around us to turn into pure, fresh drinking water – energy efficiently.
+                        </p>
+                    </div>
+                   
 
-                <p>
-                    <span className="text-blue"> Watergen’s technology extracts </span> water from the air by enabling the air to move fast into the patented Genius system in a significantly short time, ensuring greater efficiency and thus using less energy.
-                </p>
+                    <p>
+                        <span className="text-blue"> Watergen’s technology extracts </span> water from the air by enabling the air to move fast into the patented Genius system in a significantly short time, ensuring greater efficiency and thus using less energy.
+                    </p>
 
-                <p>
-                    <span className="text-blue">Unique, energy-efficient heat exchange module</span>, using condensation as a means of producing fresh water. One liter of water costs 7-15 cents, which depends on local electricity costs.
-                </p>
+                    <p>
+                        <span className="text-blue">Unique, energy-efficient heat exchange module</span>, using condensation as a means of producing fresh water. One liter of water costs 7-15 cents, which depends on local electricity costs.
+                    </p>
 
-                <p>
-                    <span className="text-blue">Highly scalable and customizable drinking water solution </span> for a wide range of daily quantities, configured to any production size. By using this technology, Watergen can supply fresh, clean drinking water to an entire town or village, or it can be scaled down to supply enough premium drinking water for private homes or offices.
-                </p>
-
-
+                    <p>
+                        <span className="text-blue">Highly scalable and customizable drinking water solution </span> for a wide range of daily quantities, configured to any production size. By using this technology, Watergen can supply fresh, clean drinking water to an entire town or village, or it can be scaled down to supply enough premium drinking water for private homes or offices.
+                    </p>
+                </StyledSectionFourContainer>
             </StyledLayoutContainer>
 
         </StyledSection>
