3f58fd422321d224efe41d120ef4c348d09e1aef
davidnjoroge <njorogedavid13@gmail.com>
2021-11-27 19:15:10 +0300

    touch up section 3 to 6

--- src/sections/Video.js
+++ src/sections/Video.js
@@ -4,8 +4,8 @@ import { StyledSection } from "../components/styled-shared";
 
 export default function Video({video}){
     return(
-        <StyledSection>
-            <video  width="100%" height="auto" controls>
+        <StyledSection className="px-5">
+            <video width="100%" height="auto" controls>
                 <source src={process.env.PUBLIC_URL + `/videos/${video}`} type="video/mp4"/>
                 Your browser does not support HTML video.
             </video>
