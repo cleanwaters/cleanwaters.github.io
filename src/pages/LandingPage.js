b6f233fc9564920f12a01c7689c8a8d8b433a20d
davidnjoroge <njorogedavid13@gmail.com>
2021-12-01 20:43:33 +0300

    add lint

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,67 +1,64 @@
-import { SectionOne } from "../sections/SectionOne";
-import { SectionTwo, StyledSectionOneDetails } from "../sections/SectionTwo";
-import { SectionThree } from "../sections/SectionThree";
-import { SectionFour } from "../sections/SectionFour";
-import { SectionFive } from "../sections/SectionFive";
-import { SectionSix } from "../sections/SectionSix";
-import { SectionMR } from "../sections/SectionMR";
-import {SectionAR} from "../sections/SectionAR";
-import { Footer } from "../sections/Footer";
+import { SectionOne } from '../sections/SectionOne';
+import { SectionTwo, StyledSectionOneDetails } from '../sections/SectionTwo';
+import { SectionThree } from '../sections/SectionThree';
+import { SectionFour } from '../sections/SectionFour';
+import { SectionFive } from '../sections/SectionFive';
+import { SectionSix } from '../sections/SectionSix';
+import { SectionMR } from '../sections/SectionMR';
+import { SectionAR } from '../sections/SectionAR';
+import { Footer } from '../sections/Footer';
 import { SectionQW } from '../sections/SectionQW';
-import { StyledBackgroundPattern } from "../App";
+import { StyledBackgroundPattern } from '../App';
 import Video from '../sections/Video';
 
-import React from "react";
-import {SectionTwoBottleAnimation} from "../sections/SectionTwoBottleAnimation";
-import {SectionOneGif} from "../sections/SectionOneGif";
-import SectionTwoCarousel from "../sections/SectionTwoCarousel";
-import {NavbarSecond} from "../components/NavbarSecond";
+import React from 'react';
+import { SectionTwoBottleAnimation } from '../sections/SectionTwoBottleAnimation';
+import { SectionOneGif } from '../sections/SectionOneGif';
+import SectionTwoCarousel from '../sections/SectionTwoCarousel';
+import { NavbarSecond } from '../components/NavbarSecond';
 
+export default function LandingPage() {
+  return (
+    <div className='container-fluid p-0'>
+      {/* <div className="container-fluid" style={{padding:'1px'}}> */}
+      <NavbarSecond />
 
-export default function LandingPage(){
-    return(
-        
-        <div className="container-fluid p-0">
-            {/* <div className="container-fluid" style={{padding:'1px'}}> */}
-                <NavbarSecond />
+      <SectionOne />
 
+      {/*<SectionTwo />*/}
 
-                <SectionOne />
+      {/*<SectionQW/>*/}
+      {/*<SectionThree />*/}
 
-                {/*<SectionTwo />*/}
+      {/*<SectionFour />*/}
 
-                {/*<SectionQW/>*/}
-                {/*<SectionThree />*/}
+      {/*<SectionFive />*/}
 
-                {/*<SectionFour />*/}
+      {/*<Video*/}
+      {/*video={'Watergen - how it works_.mp4'}*/}
+      {/*/>*/}
 
-                {/*<SectionFive />*/}
+      {/*<SectionSix />*/}
 
-                {/*<Video*/}
-                {/*video={'Watergen - how it works_.mp4'}*/}
-                {/*/>*/}
+      {/*<SectionMR />*/}
 
-                {/*<SectionSix />*/}
+      {/*<SectionAR />*/}
 
-                {/*<SectionMR />*/}
-
-                {/*<SectionAR />*/}
-
-                {/*section 10*/}
-            {/* </div> */}
-            {/*<StyledBackgroundPattern style={{top:'4500px'}}>*/}
-            {/*    <img style={{width:'120%', height:'10%'}} src={process.env.PUBLIC_URL + "/images/background/pattern3.svg"} loading="lazy" alt="image" className="img-fluid" />*/}
-            {/*</StyledBackgroundPattern>*/}
-            {/*<StyledBackgroundPattern style={{top:'5600px', left:'0%'}}>*/}
-            {/*    <img style={{width:'100%', height:'10%'}} src={process.env.PUBLIC_URL + "/images/background/pattern4.svg"} loading="lazy" alt="image" className="img-fluid" />*/}
-            {/*</StyledBackgroundPattern>*/}
-            {/*<StyledBackgroundPattern style={{top:'7200px', left:'90'}}>*/}
-            {/*    <img style={{marginLeft:'70%', width:'315px', height:'388px'}} src={process.env.PUBLIC_URL + "/images/background/pattern5.svg"} loading="lazy" alt="image" className="img-fluid" />*/}
-            {/*</StyledBackgroundPattern>*/}
-            {/*<StyledBackgroundPattern style={{top:'7400px', left:'0%'}}>*/}
-            {/*    <img style={{marginLeft:"-80px", width:'465px', height:'545px'}}  src={process.env.PUBLIC_URL + "/images/background/pattern6.svg"} loading="lazy" alt="image" className="img-fluid" />*/}
-            {/*</StyledBackgroundPattern>*/}
-            <Footer/>
-        </div>
-    )
+      {/*section 10*/}
+      {/* </div> */}
+      {/*<StyledBackgroundPattern style={{top:'4500px'}}>*/}
+      {/*    <img style={{width:'120%', height:'10%'}} src={process.env.PUBLIC_URL + "/images/background/pattern3.svg"} loading="lazy" alt="image" className="img-fluid" />*/}
+      {/*</StyledBackgroundPattern>*/}
+      {/*<StyledBackgroundPattern style={{top:'5600px', left:'0%'}}>*/}
+      {/*    <img style={{width:'100%', height:'10%'}} src={process.env.PUBLIC_URL + "/images/background/pattern4.svg"} loading="lazy" alt="image" className="img-fluid" />*/}
+      {/*</StyledBackgroundPattern>*/}
+      {/*<StyledBackgroundPattern style={{top:'7200px', left:'90'}}>*/}
+      {/*    <img style={{marginLeft:'70%', width:'315px', height:'388px'}} src={process.env.PUBLIC_URL + "/images/background/pattern5.svg"} loading="lazy" alt="image" className="img-fluid" />*/}
+      {/*</StyledBackgroundPattern>*/}
+      {/*<StyledBackgroundPattern style={{top:'7400px', left:'0%'}}>*/}
+      {/*    <img style={{marginLeft:"-80px", width:'465px', height:'545px'}}  src={process.env.PUBLIC_URL + "/images/background/pattern6.svg"} loading="lazy" alt="image" className="img-fluid" />*/}
+      {/*</StyledBackgroundPattern>*/}
+      <Footer />
+    </div>
+  );
 }
