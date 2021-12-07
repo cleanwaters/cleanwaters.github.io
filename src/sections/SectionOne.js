5c73a0efb5a7936c41e476ab6f2453b21e4221cc
Fauzan Taufik <fauzantaufik178@gmail.com>
2021-12-07 22:56:52 +0700

    second slide of jumbotron

--- src/sections/SectionOne.js
+++ src/sections/SectionOne.js
@@ -2,6 +2,7 @@ import { Button } from '../components/button/Button';
 import styled from 'styled-components';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Carousel from "react-multi-carousel";
 
 export const StyledHeaderH1 = styled.h1`
   font-weight: 800;
@@ -27,61 +28,151 @@ export const StyledSectionOneDetails = styled.div`
   margin: 20vw 0 0 7em;
 `;
 
+
+const StyledSectionOne = styled.div`
+  .react-multi-carousel-dot--active button  {
+    background-color:#0170b9 !important;
+  }
+
+  .react-multi-carousel-dot button  {
+    width:18px !important;
+    height: 18px !important;
+  }
+
+  .react-multi-carousel-dot-list {
+    bottom:5% !important;
+  }
+`
+
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 3000, min: 1400 },
+    items: 1
+  },
+  desktop: {
+      breakpoint: { max: 1400, min: 1200 },
+      items: 1
+  },
+  tablet: {
+    breakpoint: { max: 1200, min: 992 },
+    items: 1
+  },
+  mobile: {
+    breakpoint: { max: 992, min: 464 },
+    items: 1
+  }
+};
+
+
 export function SectionOne() {
   let navigate = useNavigate();
   useEffect(() => {});
 
   return (
-    <div className='jumbotron jumbotron-fluid' style={{ background: 'black' }}>
-      <video autoPlay muted loop>
-        <source
-          src={process.env.PUBLIC_URL + `/videos/production ID_3958714.mp4`}
-          type='video/mp4'
-        />
-      </video>
-      {/* <Navbar/> */}
-      <div
-        data-aos='fade-up'
-        style={{ zIndex: 4, position: 'relative', marginTop: '20vw' }}
-        className='d-md-flex'
-      >
-        <div className='col-md-6'>
-          <StyledHeaderH1
-            className='header-title-color'
-            style={{ fontSize: '48px' }}
+    <StyledSectionOne>
+      <Carousel id="section-one-lp" responsive={responsive} showDots={true} autoPlay={true} autoPlaySpeed={6000} infinite={true}>
+        <div className='jumbotron jumbotron-fluid' style={{ background: 'black' }}>
+          <video autoPlay muted loop>
+            <source
+              src={process.env.PUBLIC_URL + `/videos/production ID_3958714.mp4`}
+              type='video/mp4'
+            />
+          </video>
+          {/* <Navbar/> */}
+          <div
+            data-aos='fade-up'
+            style={{ zIndex: 4, position: 'relative', marginTop: '20vw' }}
+            className='d-md-flex'
           >
-            Clean Water
-          </StyledHeaderH1>
-          <StyledSectionOneP className='mb-3'>
-            We use Air as a natural raw material to make top-quality drinking
-            water through our atmospheric extraction technology
-          </StyledSectionOneP>
-          <div className='d-flex'>
-            <Button
-              size='large'
-              label='Our product'
-              onClick={() => navigate('/our-solution')}
-              style={{
-                color: 'white',
-                fontFamily: 'Nunito',
-                marginRight: '1em',
-                background: '#046EB6',
-              }}
-            />
-            <Button
-              size='large'
-              label='About us'
-              onClick={() => navigate('/about-us')}
-              style={{
-                color: 'white',
-                fontFamily: 'Nunito',
-                borderColor: 'white',
-              }}
-            />
+            <div className='col-md-6'>
+              <StyledHeaderH1
+                className='header-title-color'
+                style={{ fontSize: '48px' }}
+              >
+                Clean Water
+              </StyledHeaderH1>
+              <StyledSectionOneP className='mb-3'>
+                We use Air as a natural raw material to make top-quality drinking
+                water through our atmospheric extraction technology
+              </StyledSectionOneP>
+              <div className='d-flex'>
+                <Button
+                  size='large'
+                  label='Our product'
+                  onClick={() => navigate('/our-solution')}
+                  style={{
+                    color: 'white',
+                    fontFamily: 'Nunito',
+                    marginRight: '1em',
+                    background: '#046EB6',
+                  }}
+                />
+                <Button
+                  size='large'
+                  label='About us'
+                  onClick={() => navigate('/about-us')}
+                  style={{
+                    color: 'white',
+                    fontFamily: 'Nunito',
+                    borderColor: 'white',
+                  }}
+                />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+        <div className='jumbotron jumbotron-fluid' style={{ background: 'black' }}>
+          <video autoPlay muted loop>
+            <source
+              src={process.env.PUBLIC_URL + `/videos/what is sailgp123.mp4`}
+              type='video/mp4'
+            />
+          </video>
+          {/* <Navbar/> */}
+          <div
+            data-aos='fade-up'
+            style={{ zIndex: 4, position: 'relative', marginTop: '20vw' }}
+            className='d-md-flex'
+          >
+            <div className='col-md-6'>
+              <StyledHeaderH1
+                className='header-title-color'
+                style={{ fontSize: '48px' }}
+              >
+                Clean Water
+              </StyledHeaderH1>
+              <StyledSectionOneP className='mb-3'>
+                Proud Sponsor of the Sail Grand Prix
+              </StyledSectionOneP>
+              <div className='d-flex'>
+                <Button
+                  size='large'
+                  label='Our product'
+                  onClick={() => navigate('/our-solution')}
+                  style={{
+                    color: 'white',
+                    fontFamily: 'Nunito',
+                    marginRight: '1em',
+                    background: '#046EB6',
+                  }}
+                />
+                <Button
+                  size='large'
+                  label='About us'
+                  onClick={() => navigate('/about-us')}
+                  style={{
+                    color: 'white',
+                    fontFamily: 'Nunito',
+                    borderColor: 'white',
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Carousel>
+    </StyledSectionOne>
   );
 }
 
