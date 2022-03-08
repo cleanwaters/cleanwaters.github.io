e37aef756f5c19e6f10ac1793ca7cab692052c81
Paul Conyngham <Paul@MacBook-Pro.lan>
2022-03-08 18:06:45 +1100

    freebie rollback

--- src/data/aboutUsData.js
+++ src/data/aboutUsData.js
@@ -32,26 +32,28 @@ export const aboutUsData = {
         'Ofir is a global business leader, with over 20 years of experience working with government organisations, defence and large enterprises along with SME’s, successfully leading programs on a national level. He excels at building relationships across complex organisations at a C-level, while articulating the business value of technology solutions. Ofir thrives by innovation and ingenuity, and seek to embrace new technologies to benefit humanity.',
     },
     {
-      name: 'Will Jefferies - CFO',
+      name: 'Will Jefferies - Senior Advisor',
       image: '/images/aboutus/team_2.png',
       description:
         'Highly experienced communicator and political commentator with connections far and wide across the political and mineral resources landscape, both locally and internationally',
     },
     // 3
     {
-      name: 'Warren Mundine - Independent Advisor',
-      image: '/images/aboutus/team_3.png',
-      description: '"A highly respected and influential businessman, political strategist and advocate for empowering Australia’s First People, he is passionate about supporting local jobs and opportunities in Australia’s regions. Warren believes that small business is the backbone of Australia’s economy, and has successfully invested his own time and money in generating business opportunities for young Australians. Nyunggai Warren Mundine is a member of the Bundjalung, Gumbaynggirr and Yuin people of NSW."',
+      name: 'Armen Arakelian - Senior Advisor',
+      image: '/images/aboutus/about_us_section_one.png',
+      description:
+        'Start-up entrepreneur with experience across pacific trade, China im/export and commercial operations, specialising in fine foods, agribusiness and sustainability within investment, wholesale and retail trade. Armen is a long-standing volunteer with RFS and operates Back To The Bush, a non-profit social enterprise..',
     },
     // 4
     {
       name: 'Pamela Yialas - Communications and PR',
       image: '/images/aboutus/team_4.png',
-      description: "Pamela offers a full range of professional services as a consultant specialising in Event and Operation Management. She has worked with some of Australia's largest corporations and has gained an excellent project delivery reputation through global exposure. Pamela prides herself on being dynamic, diligent, and ensuring she is up to date with the latest technology in the events industry and assisting corporations to create strategies to improve productivity and efficiency.",
+      description:
+        "Pamela offers a full range of professional services as a consultant specialising in Event and Operation Management. She has worked with some of Australia's largest corporations and has gained an excellent project delivery reputation through global exposure. Pamela prides herself on being dynamic, diligent, and ensuring she is up to date with the latest technology in the events industry and assisting corporations to create strategies to improve productivity and efficiency.",
     },
     // 5
     {
-      name: 'Brian Vowels - Advisor',
+      name: 'Brian Vowels - CFO',
       image: '/images/aboutus/team_5.png',
       description:
         'An experienced finance professional having spent my entire 30 year career in the insurance and reinsurance industry working both locally and internationally in organisations ranging from start-ups to listed entities, both on the broking and underwriting side of the business.',
@@ -61,26 +63,36 @@ export const aboutUsData = {
       name: 'Alex Wymarra - Independent Advisor',
       subName: 'Representing the Treaty Council Worldwide',
       image: '/images/aboutus/team_6.png',
-      description: 'Alex Wymarra is the CEO of The Treaty Council. The Treaty Council is experienced with Indigenous land management and is passionate about helping other Clans and Nations access wealth and economic independence on country and in communities. They empower communities to grow on their own economy while culturally protecting country. The Treaty Council works with Indigenous Clans and Nations culturally, appropriately and is aware every Clan and Nation is different and has different customs, laws, wants and needs for country.',
+      description:
+        'Alex Wymarra is the CEO of The Treaty Council. The Treaty Council is experienced with Indigenous land management and is passionate about helping other Clans and Nations access wealth and economic independence on country and in communities. They empower communities to grow on their own economy while culturally protecting country. The Treaty Council works with Indigenous Clans and Nations culturally, appropriately and is aware every Clan and Nation is different and has different customs, laws, wants and needs for country.',
     },
     // 7
     {
       name: 'Bijang Slabb - Brand Ambassador',
       image: '/images/aboutus/team_7.png',
-      description: 'Bijang is an Indigenous artist, model and actor who currently works as an ambassador for Myer. He has previously worked with well-known brands such as Bonds, Billabong and Target. Bijang feels most comfortable in the water, growing up at Fingal Beach and nearby the Tweed River.',
+      description:
+        'Bijang is an Indigenous artist, model and actor who currently works as an ambassador for Myer. He has previously worked with well-known brands such as Bonds, Billabong and Target. Bijang feels most comfortable in the water, growing up at Fingal Beach and nearby the Tweed River.',
     },
     // 8
     {
       name: 'Dr Jennie Mallela - Independent Advisor',
       image: '/images/aboutus/team_8.png',
-      description: 'Dr Jennie Mallela is an environmental scientist of South American Indigenous heritage. She has two decades of experience working in Australia, Africa and the Caribbean. She conducts research that addresses real-world problems focusing on pollution and climate change. Dr Mallela works for the Australian National University and is a keen advocate for science and regularly advises government and industry in her role as an expert scientist. She has co-authored more than 40 research publications with over 130 international co-authors and her research is used by government and conservation managers.',
+      description:
+        'Dr Jennie Mallela is an environmental scientist of South American Indigenous heritage. She has two decades of experience working in Australia, Africa and the Caribbean. She conducts research that addresses real-world problems focusing on pollution and climate change. Dr Mallela works for the Australian National University and is a keen advocate for science and regularly advises government and industry in her role as an expert scientist. She has co-authored more than 40 research publications with over 130 international co-authors and her research is used by government and conservation managers.',
     },
     // 9
     {
       name: 'Antoine Jaja - Independent Advisor',
       subName: 'AKA Jack the Super Prawn',
       image: '/images/aboutus/team_9.png',
-      description: 'The Creator and Author of “Jack the Super Prawn” is Antoine Jaja. Jack the Super Prawn is an appealing vibrant super prawn ocean hero, a fun loving and friendly animated character that is known for creating awareness on the impact of plastic pollution in our environment around the world. Antoine has written several children’s books. Two of the books entitled, Drastic Plastic and Pollution Solution, will be released in paperback in bookstores throughout Australia in 2021.',
+      description:
+        'The Creator and Author of “Jack the Super Prawn” is Antoine Jaja. Jack the Super Prawn is an appealing vibrant super prawn ocean hero, a fun loving and friendly animated character that is known for creating awareness on the impact of plastic pollution in our environment around the world. Antoine has written several children’s books. Two of the books entitled, Drastic Plastic and Pollution Solution, will be released in paperback in bookstores throughout Australia in 2021.',
+    },
+    {
+      name: 'Warren Mundine - Independent Advisor',
+      image: '/images/aboutus/team_3.png',
+      description:
+        'A highly respected and influential businessman, political strategist and advocate for empowering Australia’s First People, he is passionate about supporting local jobs and opportunities in Australia’s regions. Warren believes that small business is the backbone of Australia’s economy, and has successfully invested his own time and money in generating business opportunities for young Australians. Nyunggai Warren Mundine is a member of the Bundjalung, Gumbaynggirr and Yuin people of NSW.',
     },
   ],
 };
